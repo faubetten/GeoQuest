b7372fd7f5073ea05cd35eddabf9f1f1b8e43124
João Marques <99985305+JoaoMarques03@users.noreply.github.com>
2022-12-04 18:32:08 +0000

    Update BaseDeDados.sql

--- BaseDeDados.sql
+++ BaseDeDados.sql
@@ -27,8 +27,7 @@ comment on column quest.locais_encontrados is 'The number of locations found';
 create table locais (
     locais_id SERIAL Not NULL CONSTRAINT locais_pkey PRIMARY KEY,
     coordenadas VARCHAR NOT NULL,
-    nome VARCHAR NOT NULL,
-    morada VARCHAR NULL
+    nome VARCHAR NOT NULL
 );
 
 -- Add categories locais
@@ -49,7 +48,7 @@ comment on column fotos.imagem is 'The image';
 create table quest_locais (
     quest_locais_id SERIAL NOT NULL CONSTRAINT quest_locais_pkey PRIMARY KEY,
     quest_locais_quest_id SERIAL NOT NULL REFERENCES quest (quest_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    quest_locais_locais_id SERIAL NOT NULL REFERENCES locais (locais_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    quest_locais_locais_id SERIAL NOT NULL REFERENCES locais (locais_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Add categories quest_locais
@@ -84,13 +83,13 @@ comment on column user_quest.user_quest_user_id is 'The id of the quest played b
 insert into users (users_name, users_password) values ('Roberta', '123');
 insert into users (users_name, users_password) values ('Fausto', '123');
 insert into users (users_name, users_password) values ('Mario', '123');
-insert into locais (locais_nome, locais_coordenadas) values ('Museu do Oriente', '38.7031010386998', '-9.171026243031786');
-insert into locais (locais_nome, locais_coordenadas) values ('Museu Nacional de Arte Antiga', '38.70495139178823', '-9.161488308271545');
-insert into locais (locais_nome, locais_coordenadas) values ('Jardim 9 de Abril', '38.704444', '-9.162722');
-insert into locais (locais_nome, locais_coordenadas) values ('Museu da Marioneta', '38.70810028885371', '-9.155775935408098');
-insert into locais (locais_nome, locais_coordenadas) values ('Jardim Lisboa Antiga', '38.711817422588005', '-9.154456254923248');
-insert into locais (locais_nome, locais_coordenadas) values ('Time Out Market Lisboa', '38.70722497680704', '-9.145978830266364');
-insert into locais (locais_nome, locais_coordenadas) values ('Museu das Comunicações', '38.70792824352956', '-9.15043129703937');
-insert into locais (locais_nome, locais_coordenadas) values ('Green Street Lisbon', '38.709267780011', '-9.152416131917786');
-insert into locais (locais_nome, locais_coordenadas) values ('Museu da Farmácia', '38.71025985761974', '-9.147164337525124');
-insert into locais (locais_nome, locais_coordenadas) values ('Praça Luís de Camões', '38.71074544864401', '-9.143452124801652');
+insert into locais (nome, coordenadas) values ('Museu do Oriente', '38.7031010386998,-9.171026243031786');
+insert into locais (nome, coordenadas) values ('Museu Nacional de Arte Antiga', '38.70495139178823,-9.161488308271545');
+insert into locais (nome, coordenadas) values ('Jardim 9 de Abril', '38.704444,-9.162722');
+insert into locais (nome, coordenadas) values ('Museu da Marioneta', '38.70810028885371,-9.155775935408098');
+insert into locais (nome, coordenadas) values ('Jardim Lisboa Antiga', '38.711817422588005,-9.154456254923248');
+insert into locais (nome, coordenadas) values ('Time Out Market Lisboa', '38.70722497680704,-9.145978830266364');
+insert into locais (nome, coordenadas) values ('Museu das Comunicações', '38.70792824352956,-9.15043129703937');
+insert into locais (nome, coordenadas) values ('Green Street Lisbon', '38.709267780011,-9.152416131917786');
+insert into locais (nome, coordenadas) values ('Museu da Farmácia', '38.71025985761974,-9.147164337525124');
+insert into locais (nome, coordenadas) values ('Praça Luís de Camões', '38.71074544864401,-9.143452124801652');
